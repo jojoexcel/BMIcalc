b344a723dd337085617698bd1966364dbaccd9e6
tony <jojo.excel7@gmail.com>
2023-09-24 13:02:53 +0800

    第二次commit

--- bmi.py
+++ bmi.py
@@ -1,4 +1,6 @@
 height=int(input("請輸入你的身高(cm):"))
-eeight=int(input("請輸入你的體重(Kg):"))
+weight=int(input("請輸入你的體重(Kg):"))
 bmi=weight/(height/100)**2
 print("你的身高:"+(str(height)/100)+"公尺")
+print("你的體重:"+str(weight)+"公斤")
+print("你的BMI:"+(str(round(bmi,2)))
